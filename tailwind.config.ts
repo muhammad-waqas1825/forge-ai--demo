2f4ac84fd30a18977220514708af4dbea668d7f9
muhammad-waqas1825 <ahsangujjar44201@gmail.com>
2026-05-13 00:17:28 +0500

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "tailwindcss"
+
+const config = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
